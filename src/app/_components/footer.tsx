0604899211442aa694075cd3b0a07ac265c9efe7
Nils Hansson <nilsmenghansson@gmail.com>
2024-09-25 15:02:36 +0200

    created footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,7 +1,37 @@
 export default function Footer() {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className="py-20" id="wrapper">
+      <h2 className="text-xl px-24">nilsmenghansson@gmail.com</h2>
+
+      <ul>
+        <li>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span className="fa fa-facebook"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span className="fa fa-youtube"></span>
+          </a>
+        </li>
+        <li>
+          <a href="https://instagram.com/nilshanssonmeng">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span className="fa fa-instagram"></span>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./_components/navbar";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import Footer from "./_components/footer";
 
 const archivo = Urbanist({
   subsets: ["latin"],
@@ -15,9 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={archivo.variable}>
-      <body className="font-archivo bg-customWhite">
+      <head>
+        {/* Add the FontAwesome CDN link */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
+        />
+      </head>
+      <body className="font-archivo bg-customWhite flex flex-col">
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
